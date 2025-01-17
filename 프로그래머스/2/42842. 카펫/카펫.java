b52d40d7634cdb8191fdb00ea816241d67f4c5d1
yoon hyen ho <55376180+seaking110@users.noreply.github.com>
2025-01-17 10:23:43 +0900

    [level 2] Title: 카펫, Time: 0.27 ms, Memory: 104 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+            int[] answer = new int[2];
+        for (int i = 1; i <= yellow; i++ ) {
+        	if (yellow % i == 0) {
+        		if (i * 2 + yellow/i * 2 + 4 == brown) {
+        			answer[0] = yellow / i + 2;
+        			answer[1] = i + 2;
+        			break;
+        		}
+        	}
+        }
+        System.out.println(answer[0]);
+        System.out.println(answer[1]);
+        return answer;
+    }
+}
