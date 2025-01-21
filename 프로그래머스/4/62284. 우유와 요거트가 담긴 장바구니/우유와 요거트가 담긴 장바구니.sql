f7add93d6bd155b0847e223093c0080e0b84c284
yoon hyen ho <55376180+seaking110@users.noreply.github.com>
2025-01-21 09:37:31 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,4 +1,6 @@
 -- 코드를 입력하세요
-select cart_id from (SELECT name, cart_id from cart_products c 
-group by cart_id, name having c.name = 'Yogurt' or c.name = 'Milk') as a
-group by cart_id having count(*) > 1;
+select cart_id from cart_products c 
+where c.name = 'Yogurt' or c.name = 'Milk'
+group by cart_id
+having count(distinct name) > 1
+order by cart_id
