1c8c9b040d7d1aad5c9db0fa8760ef9c6cf35973
yoon hyen ho <55376180+seaking110@users.noreply.github.com>
2025-01-23 14:18:14 +0900

    [level 2] Title: 행렬의 곱셈, Time: 9.47 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,31 +1,7 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-                int length = 0;
-        if (arr1.length == arr2.length) {
-        	int [][] answer = new int[arr1[0].length][arr2[0].length];
-        	for (int i = 0; i < arr1[0].length; i++) {
-        		for (int j = 0; j < arr2[0].length; j++) {
-        			int sum = 0;
-        			for (int l = 0; l < arr1.length; l++) {
-        				sum += arr1[i][l] * arr2[l][j];
-        			}
-        			answer[i][j] = sum;
-        		}
-        	}
-        	return answer;
-        } else if (arr1.length == arr2[0].length) {
-        	int [][] answer = new int[arr1[0].length][arr2.length];
-        	for (int i = 0; i < arr1[0].length; i++) {
-        		for (int j = 0; j < arr2.length; j++) {
-        			int sum = 0;
-        			for (int l = 0; l < arr1.length; l++) {
-        				sum += arr1[i][l] * arr2[l][j];
-        			}
-        			answer[i][j] = sum;
-        		}
-        	}
-        	return answer;
-        } else if (arr1[0].length == arr2.length) {
+               
+ 
         	int [][] answer = new int[arr1.length][arr2[0].length];
         	for (int i = 0; i < arr1.length; i++) {
         		for (int j = 0; j < arr2[0].length; j++) {
@@ -37,18 +13,6 @@ class Solution {
         		}
         	}
         	return answer;
-        } else {
-        	int [][] answer = new int[arr1.length][arr2.length];
-        	for (int i = 0; i < arr1.length; i++) {
-        		for (int j = 0; j < arr2.length; j++) {
-        			int sum = 0;
-        			for (int l = 0; l < arr1[0].length; l++) {
-        				sum += arr1[i][l] * arr2[l][j];
-        			}
-        			answer[i][j] = sum;
-        		}
-        	}
-        	return answer;
-        }
+        
     }
 }
