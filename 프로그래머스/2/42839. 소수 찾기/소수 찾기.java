beeaa49a49ed5b773506d0cb5e077dd265ef41b2
yoon hyen ho <55376180+seaking110@users.noreply.github.com>
2025-06-12 00:58:13 +0900

    [level 2] Title: 소수 찾기, Time: 24.29 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,16 +1,29 @@
 import java.util.*;
 class Solution {
     public int solution(String numbers) {
-                int answer = 0;
+        int answer = 0;
         Set<Integer> numSet = new HashSet<>();
         permute(numSet, "" , numbers);
         for (int i : numSet) {
-        	if (qwer(i)) {
+        	if (check(i)) {
         		answer++;
         	}
         }
         return answer;
     }
+    
+    public static boolean check(int a) {
+    	if (a== 0 || a == 1) {
+    		return false;
+    	}
+    	for (int i = 2; i <= Math.sqrt(a); i++) {
+    		if (a % i == 0) {
+    			return false;
+    		}
+    	}
+    	return true;
+    }
+    
     public static void permute(Set<Integer> set, String pre, String numbers) {
     	if (!pre.isEmpty()) {
     		set.add(Integer.parseInt(pre));
@@ -18,17 +31,5 @@ class Solution {
     	for (int i = 0 ; i< numbers.length(); i++) {
     		permute(set, pre+numbers.charAt(i), numbers.substring(0,i)+ numbers.substring(i+1));
     	}
-    }
-    public static boolean qwer(int x) {
-    	if (x == 0 || x == 1) {
-    		return false;
-    	} else {
-    		for (int i = 2; i <= Math.sqrt(x); i++) {
-    			if (x % i == 0) {
-    				return false;
-    			}
-    		}
-    	}
-    	return true;	
     }
 }
